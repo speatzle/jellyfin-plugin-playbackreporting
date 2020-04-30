468e1bd7065600a0e598e61c108c1275d215cdeb
AliMickey <ad312@pm.me>
2020-04-30 20:03:28 +1000

    fixed error.

--- Jellyfin.Plugin.PlaybackReporting/Pages/user_playback_report.js
+++ Jellyfin.Plugin.PlaybackReporting/Pages/user_playback_report.js
@@ -486,7 +486,7 @@ define(['libraryMenu'], function (libraryMenu) {
                     weeks.addEventListener("change", process_click);
                     //Need to implement a better method to select all data instead of 18250 days = 50 years. 
                     var days = 18250;
-                    if (parseInt(weeks.value) != 0){
+                    if (parseInt(weeks.value) != -1){
                         days = parseInt(weeks.value) * 7;
                     }
 
