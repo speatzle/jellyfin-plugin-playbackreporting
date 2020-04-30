d2e94bd45b61032c938d368d7c99cb19652a5bb8
Micky <60691199+AliMickey@users.noreply.github.com>
2020-04-30 19:56:15 +1000

    Update Jellyfin.Plugin.PlaybackReporting/Pages/user_report.js
    Co-Authored-By: Dmitry Lyzo <56478732+dmitrylyzo@users.noreply.github.com>

--- Jellyfin.Plugin.PlaybackReporting/Pages/user_report.js
+++ Jellyfin.Plugin.PlaybackReporting/Pages/user_report.js
@@ -85,7 +85,7 @@ define(['libraryMenu'], function (libraryMenu) {
                 //Need to implement a better method to select all data instead of 18250 days = 50 years. 
                 var days = 18250;
                 if (parseInt(weeks.value) != -1){
-                    var days = parseInt(weeks.value) * 7;
+                    days = parseInt(weeks.value) * 7;
                 }
                 var url = "user_usage_stats/user_activity?days=" + days + "&end_date=" + end_date.value + "&stamp=" + new Date().getTime();
                 url = ApiClient.getUrl(url);
