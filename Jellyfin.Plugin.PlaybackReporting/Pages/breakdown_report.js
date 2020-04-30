19e43ea9f31517015f09db0a98c1e80f0f9c3df8
AliMickey <ad312@pm.me>
2020-04-30 20:38:58 +1000

    days change

--- Jellyfin.Plugin.PlaybackReporting/Pages/breakdown_report.js
+++ Jellyfin.Plugin.PlaybackReporting/Pages/breakdown_report.js
@@ -268,7 +268,7 @@ define(['libraryMenu'], function (libraryMenu) {
                     //Need to implement a better method to select all data instead of 18250 days = 50 years. 
                     var days = 18250;
                     if (parseInt(weeks.value) != -1){
-                        var days = parseInt(weeks.value) * 7;
+                        days = parseInt(weeks.value) * 7;
                     }
                     
                     // build user chart
